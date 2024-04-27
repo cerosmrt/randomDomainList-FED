2ac21c4797087a8a1b109a8e21187605284a8e6f
shlimsady <federicomoret23@gmail.com>
2024-04-27 18:45:18 -0300

    recommit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,29 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const reloadButton = document.getElementById("reloadButton");
+  const mainText = document.getElementById("main-text");
+
+  reloadButton.addEventListener("click", function() {
+    mainText.innerHTML = generateDomain();
+  });
 };
+
+function generateDomain() {
+  let pronouns = ["the", "our", "your", "my", "his", "her", "their"];
+  let adjectives = ["flying", "dancing", "whispering", "shining", "ancient"];
+  let nouns = ["phoenix", "unicorn", "griffin", "dragon", "sphinx"];
+  let extensions = [".com", ".net", ".us", ".io"];
+  
+  let domains = [];
+
+  for (let i = 0; i < pronouns.length; i++) {
+    for (let j = 0; j < adjectives.length; j++) {
+      for (let k = 0; k < nouns.length; k++) {
+        for (let l = 0; l < extensions.length; l++) {
+          domains.push(pronouns[i] + adjectives[j] + nouns[k] + extensions[l]);
+        }
+      }
+    }
+  }
+
+  return domains.join('<br>');
+}
